2b05db6befc1d2b1081f381224c5addcd5d70eef
grahamwork8 <grahamwork8@gmail.com>
2025-10-02 15:53:41 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['next', 'next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     'react-hooks/exhaustive-deps': 'warn',
   },
