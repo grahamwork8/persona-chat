f3b7fda29024fb2baf9ee21358ca5258251613ca
grahamwork8 <grahamwork8@gmail.com>
2025-10-03 21:32:43 -0400

    Ensure Clerk middleware is deployed

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,6 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    "/((?!.*\\..*|_next).*)", // all pages except static files
-    "/",                      // root
-    "/(api|trpc)(.*)"         // all API routes
-  ]
+  matcher: ["/(.*)"],
 };
 
